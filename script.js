19dbfa0a869a50257aeedbeaef53dc9c07aa1f44
Lucas Viacava <lucas.viacava@hotmail.com>
2024-06-12 11:43:23 -0300

    feat: Add algorithm to sort array
    The code changes introduce a new algorithm to sort an array. The `Algorithm` function shuffles the array until it is sorted using the `shuffle` and `isSorted` helper functions. The purpose of this change is to provide a different approach to sorting arrays.

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ function quakeFastInverseSqrt(x) {
 
     return y;
 }
+
+function Algorithm(arr){
+    var isSorted = function(arr){
+        for(var i = 1; i < arr.length; i++){
+            if (arr[i-1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    function shuffle(arr){
+        var count = arr.length, temp, index;
+
+        while(count > 0){
+            index = Math.floor(Math.random() * count);
+            count--;
+
+            temp = arr[count];
+            arr[count] = arr[index];
+            arr[index] = temp;
+        }
+
+        return arr;
+    }
+
+    function sort(arr){
+        var sorted = false;
+        while(!sorted){
+            arr = shuffle(arr);
+            sorted = isSorted(arr);
+        }
+        return arr;
+    }
+
+    return sort(arr);
+}
+
+function check() { 
+    const start = performance.now()
+    let arra = [1,5,3,2,1,65,123,-12,3,-4,99,-1293,924154,-1928312,123]
+    Algorithm(arra)
+    const end = performance.now()
+    return `Execution time: ${end - start} ms`
+}
